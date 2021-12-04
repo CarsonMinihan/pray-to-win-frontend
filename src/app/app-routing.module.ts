4b795bdf253fe9eb7668086967d652b54651d1ef
mydubs <my.dubs@gmail.com>
2021-12-03 21:42:12 -0600

    Restructured folders based on app discussion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CalendarComponent } from './components/calendar/calendar.component';
-import { DailyMoodChangesComponent } from './components/daily-mood-changes/daily-mood-changes.component';
-import { JournalComponent } from './components/journal/journal.component';
-import { LoginComponent } from './components/login/login.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
+import { DailyMoodChangesComponent } from './pages/daily-mood-changes/daily-mood-changes.component';
+import { JournalComponent } from './pages/journal/journal.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
-  {path: "", redirectTo: "calendar", pathMatch: "full"},
-  {path: "calendar", component: CalendarComponent},
-  {path: "login", component: LoginComponent},
-  {path: "journal", component: JournalComponent},
-  {path: "dailymood", component: DailyMoodChangesComponent},
+  { path: '', redirectTo: 'calendar', pathMatch: 'full' },
+  { path: 'calendar', component: CalendarComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'journal', component: JournalComponent },
+  { path: 'dailymood', component: DailyMoodChangesComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { CalendarComponent } from './components/calendar/calendar.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 import { FlatpickrModule } from 'angularx-flatpickr';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LoginComponent } from './components/login/login.component';
-import { DailyMoodChangesComponent } from './components/daily-mood-changes/daily-mood-changes.component';
-import { JournalComponent } from './components/journal/journal.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DailyMoodChangesComponent } from './pages/daily-mood-changes/daily-mood-changes.component';
+import { JournalComponent } from './pages/journal/journal.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/navbar/navbar.component';
-
 
 @NgModule({
   declarations: [
@@ -25,20 +24,23 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     LoginComponent,
     DailyMoodChangesComponent,
     JournalComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory }),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
     NgbModalModule,
     FlatpickrModule.forRoot(),
     NgbModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
